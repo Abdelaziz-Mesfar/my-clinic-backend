e52fbb37971147be936a5d19db99232fc0320d0a
Abdelaziz Mesfar <abdelaziz.mesfar01@gmail.com>
2022-03-26 22:37:44 +0100

    connect the server to the mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose')
 require('dotenv').config();
 
 const app = express();
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.json('welcome to my cabinet api')
 })
 
-const PORT = process.env.PORT;
-app.listen(PORT, ()=>{
-    console.log(`the port is listening on port ${PORT}`);
-})
+
+const PORT = process.env.PORT || 7000;
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true})
+    .then(() => {
+        console.log('successfully connected to mongodBD');
+        app.listen(PORT, () => {
+            console.log(`the port is listening on port ${PORT}`);
+        })
+    })
+    .catch(error => {
+        console.log(error);
+    })
